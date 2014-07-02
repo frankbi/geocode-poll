442f4181b636803595bb97d0192f8a68c3ce0631
Frank Bi <fbi@newshour.org>
2014-07-02 11:42:41 -0400

    combine data return functions

--- geo.php
+++ geo.php
@@ -1,32 +1,55 @@
 <?php
 
-include "creds.php";
-include "functions.php";
+	include "creds.php";
+	include "functions.php";
 
 
-function return_from_sql($c) {
+	function return_poll_results($c) {
 
-	$result = mysqli_query($c, "
-		SELECT ans, COUNT(*) AS ans_num
-		FROM geo_korm_07012014
-		GROUP  BY ans;
-	");
+		$result = mysqli_query($c, "
+			SELECT ans, COUNT(*) AS ans_num
+			FROM geo_korm_07012014
+			GROUP BY ans;
+		");
 
-	while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-		print_r($row);
+		$json = array();
+		while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+			array_push($json, $row);
+		}
+
+		return json_encode($json);
+
 	}
 
-}
+	function return_geocode_results($c) {
+
+		$result = mysqli_query($c, "
+			SELECT region_name, COUNT(*) AS region_name_count
+			FROM geo_korm_07012014
+			GROUP BY region_name; 
+		");
+
+		$json = array();
+		while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+			array_push($json, $row);
+		}
+
+		return json_encode($json);
+
+	}
 
 
 
 
-$con = connect_to_sql();
-$data = get_ip();
-// insert_to_sql($con, $data, $_POST["ans"]);
-insert_to_sql($con, $data, 0);
+	$con = connect_to_sql();
+	$data = get_ip();
+	insert_to_sql($con, $data, $_POST["ans"]);
+	
+	// insert_to_sql($con, $data, 0);
 
-return_from_sql($con);
+	// echo return_poll_results($con);
+
+	echo return_geocode_results($con);
 
 
 ?>
