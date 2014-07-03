9587fafc5bd10d66e07184b0930885bfacd5bb40
Frank Bi <fbi@newshour.org>
2014-07-03 14:49:31 -0400

    fuck style

---
+++ post.php
@@ -0,0 +1,24 @@
+<?php
+
+	// contains database login info
+	include "creds.php";
+
+	// all functions here
+	include "functions.php";
+
+	// selection made by user
+	$ans = $_POST["ans"];
+
+	// create connection to sql
+	$con = connect_to_sql();
+
+	// returns ip and other relevant info
+	$data = get_ip();
+
+	// insert data into sql
+	insert_to_sql($con, $data, $ans);
+
+	// return data as json
+	echo return_results($con);
+
+?>
